a2f4cb26dba70145f64d22e90fbb7d014fe069cc
Desai Riddhi <desairiddhi15@gmail.com>
2022-04-01 23:14:36 +0530

    change word

--- bin/palindrome.dart
+++ bin/palindrome.dart
@@ -1,14 +1,15 @@
 import'dart:io';
 
 void main(){
-  print("enter word :");
-  String? original=stdin.readLineSync();
+
+    print("enter the word or number:");
+    String? original=stdin.readLineSync();
 
   String? reverse=original!.split('').reversed.join('');
   if(original==reverse){
     print("it's palindrome");
   }
   else{
-    print("it's palindrome");
+    print("it's not palindrome");
   }
 }
